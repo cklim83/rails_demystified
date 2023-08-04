b0f38afd563569525afb3646b07e31e2bc72215e
cklim <cheekiang.lim@gmail.com>
2023-08-04 20:39:25 +0800

    data encapsulation with models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   def list_posts
-    posts = connection.execute("SELECT * FROM posts")
+    posts = Post.all
 
     render 'application/list_posts', locals: { posts: posts }
   end
 
   def show_post
-    post = find_post_by_id(params[:id])
+    post = Post.find(params[:id])
 
     render 'application/show_post', locals: { post: post }
   end
@@ -16,59 +16,35 @@ class ApplicationController < ActionController::Base
   end
 
   def create_post
-    insert_query = <<~SQL
-      INSERT INTO posts (title, body, author, created_at)
-      VALUES (?, ?, ?, ?)
-    SQL
-
-    connection.execute insert_query,
-      params[:title],
-      params[:body],
-      params[:author],
-      Date.current.to_s
-
+    post = Post.new('title' => params[:title],
+                    'body' => params[:body],
+                    'author' => params[:author])
+    post.save
     redirect_to '/list_posts'
   end
 
   def edit_post
-    post = find_post_by_id(params[:id])
+    post = Post.find(params[:id])
 
     render 'application/edit_post', locals: { post: post }
   end
 
   def update_post
-    update_query = <<~SQL
-      UPDATE posts
-      SET title = ?,
-          body = ?,
-          author = ?
-      WHERE posts.id = ?
-    SQL
-
-    connection.execute update_query,
-      params[:title],
-      params[:body],
-      params[:author],
-      params[:id]
+    post = Post.find(params[:id])
+    post.set_attributes(
+      'title' => params[:title],
+      'body' => params[:body],
+      'author' => params[:author]
+    )
+    post.save
 
     redirect_to '/list_posts'
   end
 
   def delete_post
-    connection.execute("DELETE FROM posts WHERE posts.id = ?", params[:id])
+    post = Post.find(params[:id])
+    post.destroy
   
     redirect_to '/list_posts'
-  end
-
-  private
-
-  def connection
-    db_connection = SQLite3::Database.new 'db/development.sqlite3'
-    db_connection.results_as_hash = true
-    db_connection  
-  end
-
-  def find_post_by_id(id)
-    connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", id).first
   end
 end
